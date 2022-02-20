6f58d4342d5c1755e6a2d74787b6507b04df1824
Chau Pham <ncp9988@gmail.com>
2022-02-20 22:37:47 +0700

    user model exported

---
+++ models/User.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        trim: true,
+        required: 'Username is required',
+    },
+    email: {
+        type: String,
+        require: 'Email is required',
+        unique: true,
+        validation: match[`/.+\@.+\..+/`],
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
+
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
