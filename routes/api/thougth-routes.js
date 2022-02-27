535030cf8ea618a37a04c67e29572f8b13afe869
Chau Pham <ncp9988@gmail.com>
2022-02-27 10:35:04 -0600

    created thought routes

--- routes/api/thougth-routes.js
+++ routes/api/thougth-routes.js
@@ -4,13 +4,38 @@ const { Thought } = require('../../models');
 
 routes.get("/", (req, res) => {
     thought.find().select('-__v')
-    .then(allthoughts =>  res.json(allthoughts))
-    .catch(error => res.status(500).json(error));
-   
-   });
+        .then(allthoughts => res.json(allthoughts))
+        .catch(error => res.status(500).json(error));
+
+});
 routes.get("/:thoughtId", (req, res) => {
-    Thought.findOne({ _id:req.params.userId}).select('-__v')
+    Thought.findOne({ _id: req.params.thoughtId }).select('-__v')
+        .then(allthoughts => res.json(allthoughts))
+        .catch(error => res.status(500).json(error));
+
+});
+
+routes.post("/", (req, res) => {
+    Thought.create(req.body)
+        .then(allthoughts => res.json(allthoughts))
+        .catch(error => res.status(500).json(error));
+
+});
+
+routes.put("/:thoughtId", (req, res) => {
+    Thought.findOneAndUpdate({ _id: req.params.thoughtId },
+        { $set: req.body }, { new: true })
+        .then(allthoughts => res.json(allthoughts))
+        .catch(error => res.status(500).json(error));
+
+});
+
+routes.delete("/:thoughtId", (req, res) => {
+    Thought.findOneAndDelete({ _id:req.params.thoughtId })
     .then(allthoughts =>  res.json(allthoughts))
     .catch(error => res.status(500).json(error));
    
    });
+
+module.exports = routes;
+
